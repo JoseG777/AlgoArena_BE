abd8b8ed9a6c71427b92dcaafd6f3c899b832313
JoseG777 <josegc7773@gmail.com>
2025-09-23 18:00:24 -0400

    Create User model

--- src/server.ts
+++ src/server.ts
@@ -1,19 +1,24 @@
 import express from "express";
 import cors from "cors";
 import { connectDb } from "./lib/db_connect";
+import { User } from "./model/User";
 
 const app = express();
 const port = 3001;
 app.use(cors({
   origin: "*"
 }));
-connectDb();
 
 app.get("/", (_req, res) => {
   console.log("Hello, Algo Arena Backend!");
   res.send("Hello, Algo Arena Backend!");
 });
 
-app.listen(port, () => {
-  console.log(`Backend running at http://localhost:${port}`);
-});
+(async () => {
+  await connectDb();
+  await User.init();
+
+  app.listen(port, () => {
+    console.log(`Backend running at http://localhost:${port}`);
+  });
+})();
