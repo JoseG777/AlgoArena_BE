c9f37910c9a83a0d5d054214fd587314e58b5841
JoseG777 <josegc7773@gmail.com>
2025-09-23 19:17:21 -0400

    Complete sign up logic

--- src/routes/register.ts
+++ src/routes/register.ts
@@ -2,14 +2,50 @@ import { Router } from "express";
 import argon2 from 'argon2';
 import { User } from "../model/User";
 
-const router = Router();
+const registerRoute = Router();
 
 const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
 const usernameRegex = /^[a-zA-Z0-9]+$/;
 
-router.post("/register", async (req, res) => 
+registerRoute.post("/register", async (req, res) => 
 {
-    console.log("Place holding");
+    const {username, email, password} = req.body;
+
+    if(!username || !usernameRegex.test(username))
+    {
+        return res.status(400).json({
+            error: "Invalid Username"
+        })
+    }
+
+    if(!email || !emailRegex.test(email))
+    {
+        return res.status(400).json({
+            error: "Invalid Email"
+        })
+    }
+
+    if(!password || password.length < 8)
+    {
+        return res.status(400).json({
+            error: "Invalid Password"
+        })
+    }
+
+    const exists = await User.findOne({$or:[{username}, {email}]});
+    if(exists)
+    {
+        return res.status(400).json({
+            error: "Username or Email already in use"
+        })
+    }
+
+    const passwordHash = await argon2.hash(password);
+    await User.create({username, email, passwordHash});
+
+    res.status(201).json({
+        message: "User successfully registered!"
+    })
 });
 
-export default router;
+export default registerRoute;
--- src/server.ts
+++ src/server.ts
@@ -2,8 +2,10 @@ import express from "express";
 import cors from "cors";
 import { connectDb } from "./lib/db_connect";
 import { User } from "./model/User";
+import registerRoute from "./routes/register";
 
 const app = express();
+app.use(express.json()); 
 const port = 3001;
 app.use(cors({
   origin: "*"
@@ -15,6 +17,8 @@ app.get("/", (_req, res) => {
   console.log("Hello, Algo Arena Backend!");
   res.send("Hello, Algo Arena Backend!");
 });
+
+app.use(registerRoute);
 
 app.listen(port, () => {
   console.log(`Backend running at http://localhost:${port}`);
