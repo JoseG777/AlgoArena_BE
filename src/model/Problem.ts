afadb7a743a54594ab17a9cad68c69336d2c0a92
JoseG777 <josegc7773@gmail.com>
2025-10-30 19:35:35 -0400

    Add problems model and script to upload to mongodb

---
+++ src/model/Problem.ts
@@ -0,0 +1,41 @@
+import { Schema, model } from 'mongoose';
+
+type Lang = 'typescript' | 'python';
+type Difficulty = 'easy' | 'medium' | 'hard';
+
+interface IProblem {
+  problemId: string;
+  title: string;
+  difficulty: Difficulty;
+
+  startingCode: Record<Lang, string>;
+  testHarness: Record<Lang, string>;
+}
+
+const LangBlockSchema = new Schema<Record<Lang, string>>(
+  {
+    typescript: { type: String, required: true },
+    python: { type: String, required: true },
+  },
+  { _id: false },
+);
+
+const ProblemSchema = new Schema<IProblem>(
+  {
+    problemId: { type: String, required: true, unique: true, index: true },
+    title: { type: String, required: true },
+    difficulty: {
+      type: String,
+      enum: ['easy', 'medium', 'hard'],
+      required: true,
+      index: true,
+    },
+    startingCode: { type: LangBlockSchema, required: true },
+    testHarness: { type: LangBlockSchema, required: true },
+  },
+  {
+    collection: 'problems',
+  },
+);
+
+export const Problem = model<IProblem>('Problem', ProblemSchema);
