cf7e2c61ceec1fbc1d7f0e1ec2574580b5b9a06a
JoseG777 <josegc7773@gmail.com>
2025-11-05 19:33:49 -0500

    Create script to update existing users
    Used ChatGPT to generate

---
+++ src/scripts/updateUsers.ts
@@ -0,0 +1,31 @@
+import { connectDb } from '../lib/db_connect';
+import { User } from '../model/User';
+
+async function run() {
+  connectDb();
+
+  const result = await User.updateMany(
+    {
+      $or: [
+        { stats: { $exists: false } },
+        { 'stats.totalPoints': { $exists: false } },
+      ],
+    },
+    {
+      $set: {
+        'stats.totalPoints': 0,
+        'stats.matches': 0,
+        'stats.wins': 0,
+        'stats.losses': 0,
+        'stats.ties': 0,
+      },
+    },
+  );
+
+  console.log(`Updated ${result.modifiedCount} users.`);
+}
+
+run().catch((err) => {
+  console.error(err);
+  process.exit(1);
+});
